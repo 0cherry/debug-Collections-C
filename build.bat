13973130108f08749b3c7d9aaf962d7fdf415689
Yeongcheol Kim <aree91@naver.com>
2025-03-05 16:51:08 +0900

    reset buid directory

--- build.bat
+++ build.bat
@@ -1,6 +1,4 @@
-if not exist build (
-	mkdir build
-	cmake -S . -B build -G"MinGW Makefiles"
-) 
-
+if exist build rmdir /s /q build
+mkdir build
+cmake -S . -B build -G"MinGW Makefiles"
 cmake --build build -j4
